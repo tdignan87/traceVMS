ee56a1bea262f9a536359d3b1466288f21b47e89
Tom <tdignan87@gmail.com>
2020-04-28 22:08:47 +0100

    added timestamp to table entry

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask, render_template, url_for, request, redirect, session
 import os
 import bcrypt
 from flask_pymongo import PyMongo
+from datetime import datetime
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
-
 
 app = Flask (__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
-
-
 
 @app.route("/")
 def home():
@@ -33,13 +31,15 @@ def adminlogin():
 
 @app.route("/add_visitor",methods=['POST'])
 def add_visitor():
+    
+    
+    dateTimeObj = datetime.now()
     add_new_visitor = {"name": request.form.get("visitorName"),
                        "company":request.form.get("visitorCompany"),
-                       "visiting":request.form.get("visitorRepresent")}
+                       "visiting":request.form.get("visitorRepresent"),
+                       "sign_in_timestamp": dateTimeObj}
     mongo.db.visitors.insert_one(add_new_visitor)
     return render_template("main.html")
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
