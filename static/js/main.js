0832e8e3d5fd5305f808f05f61f5beef0d4a976b
Tom <tdignan87@gmail.com>
2020-04-28 21:11:06 +0100

    Added comments to JS

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,12 @@ window.onload = function() {
     $("#questionaire-set").hide();
     $("#contractor-section").hide();
     $("#visit-only-btn").hide();
+    $("#confirmation-container").hide();
 }
+
+/**
+ * hide sections of the page and only show depending on whats selected on the radio button
+ */
 
 $('input:radio[name="gridRadios"]').change(
     function() {
@@ -14,9 +19,15 @@ $('input:radio[name="gridRadios"]').change(
             $("#questionaire-set").show();
             $("#contractor-section").show();
             $("#visit-only-btn").hide();
+            $("#confirmation-container").show();
         } else {
             $("#questionaire-set").hide();
             $("#contractor-section").hide();
             $("#visit-only-btn").show();
+            $("#confirmation-container").hide();
         }
     });
+
+/**
+ * DateTime.Now to main form showing clock for Visitor.
+ */
