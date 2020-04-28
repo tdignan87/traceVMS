43c6c928d63ff4e7b2d10f60cd01cae4b2c5e430
Tom <tdignan87@gmail.com>
2020-04-28 18:43:59 +0100

    added show/hide for radio buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/**
+ * Hide questions section of signin page if visiting bakery checkbox is not checked.
+ */
+
+window.onload = function() {
+    $("#questionaire-set").hide();
+    $("#contractor-section").hide();
+
+}
+
+$('input:radio[name="gridRadios"]').change(
+    function() {
+        if (this.checked && this.value == 'yesOption') {
+            console.log("yes selected");
+            $("#questionaire-set").show();
+            $("#contractor-section").show();
+        } else {
+            $("#questionaire-set").hide();
+            $("#contractor-section").hide();
+        }
+    });
