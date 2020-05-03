4681e30f742eb2c895a18f37dfc92490e383102c
Tom <tdignan87@gmail.com>
2020-05-03 19:37:45 +0100

    added variables to remove duplication

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 ### Constants ###
+question_table = mongo.db.av_questions
+contractor_table = mongo.db.contractors
+visitor_table = mongo.db.visitors
+
 
 @app.route("/")
 def home():
@@ -28,8 +32,8 @@ def admin():
 @app.route("/sign_in")
 def signin():
     return render_template("signin.html",
-                           av_questions=mongo.db.av_questions.find(),
-                           contractors=mongo.db.contractors.find())
+                           av_questions=question_table.find(),
+                           contractors=contractor_table.find())
 
 
 """Administrator login page to access admin menu for CRUD"""
@@ -66,14 +70,14 @@ def add_visitor():
 @app.route("/edit_visitor")
 def edit_visitor():
     return render_template("edit-visitor.html",
-                           visitors=mongo.db.visitors.find(),
-                           contractors=mongo.db.contractors.find())
+                           visitors=visitor_table.find(),
+                           contractors=contractor_table.find())
     
 """Update visitors based on input from the edit visitors page."""
 @app.route("/amend_visitor",methods=['POST'])
 def amend_visitor():
     visitor_id = request.form.get("chooseName")
-    update = mongo.db.visitors
+    update = visitor_table
     update.update_one ({"_id": ObjectId(visitor_id)},
                        {"$set":{
                             "company": request.form.get("visitorCompany"),
@@ -84,13 +88,13 @@ def amend_visitor():
 @app.route("/delete")
 def delete():
     return render_template("delete-visitor.html",
-                           visitors=mongo.db.visitors.find())                  
+                           visitors=visitor_table.find())                  
 
 """Delete visitor from the system"""
 @app.route("/delete_visitor",methods=['POST'])
 def delete_visitor():
     visitor_id = request.form.get("deleteName")
-    mongo.db.visitors.remove({"_id": ObjectId(visitor_id)})
+    visitor_table.remove({"_id": ObjectId(visitor_id)})
     return redirect(url_for("admin"))
 
 @app.route("/add_company")
@@ -103,7 +107,7 @@ def insert_company():
     company_doc = {"Name": request.form.get("newCompanyName"),
                    "Address": request.form.get("newCompanyAddress"),
                     "approved":request.form.get("approveRadios")}
-    mongo.db.contractors.insert_one(company_doc)
+    contractor_table.insert_one(company_doc)
     return redirect(url_for("admin"))
 
 @app.route("/edit_company")
@@ -114,7 +118,7 @@ def edit_company():
 @app.route("/update_company",methods=['POST'])
 def update_company():
     company_id = request.form.get("chooseCompany")
-    update = mongo.db.contractors
+    update = contractor_table
     update.update_one({"_id": ObjectId(company_id)},
                       {"$set": {
                           "Address": request.form.get("editCompAddress"),
@@ -125,19 +129,15 @@ def update_company():
 @app.route("/delete_company")
 def delete_company():
     return render_template("delete-company.html",
-                           contractors=mongo.db.contractors.find())
+                           contractors=contractor_table.find())
 
 @app.route("/delete_company_record",methods=['POST'])
 def delete_company_record():
     company_id = request.form.get("deleteCompany")
-    mongo.db.contractors.remove({'_id': ObjectId(company_id)})
+    contractor_table.remove({'_id': ObjectId(company_id)})
     return redirect(url_for("admin"))   
     
-    
-    
-    
-    
-    
+
 @app.route("/insert_question")
 def insert_question():
     return render_template("add-questions.html")
@@ -148,13 +148,13 @@ def add_question():
     add_new_question = {"Question": request.form.get("newQuestionAdd"),
                         "Answer_First": request.form.get("newAnswerAdd"),
                         "Answer_Second": request.form.get("newAnswerAddSecond")}
-    mongo.db.av_questions.insert_one(add_new_question)
+    question_table.insert_one(add_new_question)
     return redirect(url_for("admin"))
 
 @app.route("/sign_out")
 def sign_out():
     return render_template("sign-out.html",
-                           visitors=mongo.db.visitors.find())
+                           visitors=visitor_table.find())
     
     
     """ Update visitor table by adding in signed out timestamp """
@@ -162,7 +162,7 @@ def sign_out():
 def sign_out_visitor():
     visitor_id = request.form.get("signOutName")
     dateTimeObj = datetime.now()
-    signout = mongo.db.visitors
+    signout = visitor_table
     signout.update_one ({"_id": ObjectId(visitor_id)},
                            {"$set":{
         "sign_out_timestamp":dateTimeObj 
@@ -170,18 +170,15 @@ def sign_out_visitor():
     return redirect(url_for("home"))
                     
 
-                                           
-
-
 @app.route("/edit_question")
 def edit_question():
     return render_template("edit-question.html",
-                           av_questions=mongo.db.av_questions.find())
+                           av_questions=question_table.find())
 
 @app.route("/update_question",methods=['POST'])
 def update_question():
     question_id = request.form.get("amendQuestion")
-    update = mongo.db.av_questions
+    update = question_table
     update.update_one({"_id": ObjectId(question_id)},
                       {"$set": {
                         "Question": request.form.get("editQuestionAdd"),
@@ -193,15 +190,13 @@ def update_question():
 @app.route("/delete_question")
 def delete_question():
     return render_template("delete-question.html",
-                           av_questions=mongo.db.av_questions.find())
+                           av_questions=question_table.find())
 
 @app.route("/remove_question",methods=['POST'])
 def remove_question():
     question_id = request.form.get("removeQuestion")
-    mongo.db.av_questions.remove({"_id": ObjectId(question_id)})
+    question_table.remove({"_id": ObjectId(question_id)})
     return redirect(url_for("admin"))
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
