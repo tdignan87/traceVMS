a50100a0c60ffce97ab33858e52c858398054b88
Tom <tdignan87@gmail.com>
2020-05-04 21:54:33 +0100

    added link to key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if os.path.exists("env.py"):
 app = Flask (__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
