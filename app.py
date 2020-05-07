754c335b7e0dae05b44ba901b0a3b163201dc4b8
Tom <tdignan87@gmail.com>
2020-05-07 23:07:44 +0100

    added IP and port path

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app = Flask (__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["IP"] = os.environ.get("IP")
+app.config["PORT"] = os.environ.get("PORT")
 
 MONGO = PyMongo(app)
 
