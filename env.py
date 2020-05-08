bd3bcfe2dfd519338aface8f33e7999edb2db2b0
Tom <tdignan87@gmail.com>
2020-05-08 10:52:18 +0100

    removed env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["MONGODB_NAME"] = "vms"
-os.environ["MONGO_URI"] = "mongodb+srv://root:P4Wnet95pal@myfirstcluster-8kwur.mongodb.net/vms?retryWrites=true&w=majority"
-os.environ["SECRET_KEY"] = "r00t753"
-os.environ["IP"] = "0.0.0.0"
-os.environ["PORT"] = "5000"
